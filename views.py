daea1747fba9377ff83c28fee2f3c4e4b5affbf2
RussoTl <tomasrusso1999@gmail.com>
2023-07-20 20:45:29 -0300

    primero

--- views.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-class MainMenuView(tk.Frame):
-    def __init__(self, parent, destinos_culinarios, actividades, rutas_visita):
-        super().__init__(parent)
-        self.destinos_culinarios = destinos_culinarios
-        self.actividades = actividades
-        self.rutas_visita = rutas_visita
