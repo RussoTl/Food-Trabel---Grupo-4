574e20536dc81228cd54123a2b1d17d61dbd9b6f
RussoTl <tomasrusso1999@gmail.com>
2023-07-20 18:39:07 -0300

    main

--- main.py
+++ main.py
@@ -1,4 +1,2 @@
 #Este va a ser el archivo PRINCIPAL
 #Aqui vamos a crear la ventana principal.
-#def o class necesarias para cargar los datos, mostrar las vistas y gestionar las interacciones del usuario.
-
